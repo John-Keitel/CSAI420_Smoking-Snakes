10bc544cd953ffa0e320eca532eed6ac4681abb9
asf0 <scorpionspfc@gmail.com>
2026-07-04 11:23:57 -0600

    feat(platform): upgrade to Node 24 and Next.js 16

--- src/lib/logger/types.ts
+++ src/lib/logger/types.ts
@@ -1,6 +1,10 @@
 export type Logger = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     debug: (message: string, ...args: any[]) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     error: (message: string, ...args: any[]) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     info: (message: string, ...args: any[]) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     warn: (message: string, ...args: any[]) => void;
 };
